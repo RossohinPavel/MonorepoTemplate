48aee7c99c463de8436a1f0c8083c7b63e29ac5f
dfx_17 <rossohinpavel@yandex.ru>
2026-03-01 12:14:36 +0300

    nestia types

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Logger } from '@nestjs/common';
+import { INestiaConfig, NestiaSwaggerComposer } from '@nestia/sdk';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Fastify core
@@ -14,6 +16,16 @@ async function bootstrap() {
     logger.log(`${req.ip} "${req.method} ${req.url}" ${res.statusCode}`);
     done();
   });
+
+  // Swagger
+  const config: Omit<INestiaConfig.ISwaggerConfig, "output"> = {
+    info: {
+      title: "Backend",
+    },
+    servers: [{url: "http://localhost:3000", description: "Localhost"}]
+  }
+  const document = await NestiaSwaggerComposer.document(app, config);
+  SwaggerModule.setup("docs", app, document as OpenAPIObject);
 
   await app.listen(3000, '0.0.0.0');
 }
