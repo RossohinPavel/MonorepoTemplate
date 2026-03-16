38feb1ab7faf985df193f7d1512c3896a2c0c349
dfx_17 <rossohinpavel@yandex.ru>
2026-03-16 12:39:10 +0300

    some minore changes to improve template

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,31 @@
 import { AppService } from "./app.service";
 import { TypedRoute } from "@nestia/core";
 import { Controller } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 
 
+@ApiTags('App')
 @Controller()
 export class AppController {
 
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly service: AppService) {}
 
+  /**
+   * @summary Пингуем сервис.
+   * @returns pong
+   */
+  @TypedRoute.Get("ping")
+  getPing(): string {
+    return this.service.ping();
+  }
+
+  /**
+   * @summary Получает значение из функции shared саб-сервиса.
+   * @returns pong
+   */
   @TypedRoute.Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.service.getHello();
   }
 
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,6 +5,10 @@ import * as shared from "@project/shared";
 @Injectable()
 export class AppService {
 
+  ping(): string {
+    return "pong";
+  }
+
   getHello(): string {
     return shared.sayHello();
   }
